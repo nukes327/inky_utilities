790b4074ac880a93590f66ca858c1f63407f0ee0
nukes327 <jake.nukes327@gmail.com>
2019-07-05 15:24:14 -0400

    Quick and easy display program to see if spritework is satisfactory

---
+++ display-test.py
@@ -0,0 +1,9 @@
+from inky import InkyPHAT
+from PIL import Image
+
+inky_display = InkyPHAT('yellow')
+inky_display.set_border(inky_display.BLACK)
+
+img = Image.open('test-screen.png')
+inky_display.set_image(img)
+inky_display.show()
