a44dc3d61d8539c6f8f743b543c80f5befd2207d
nukes327 <jake.nukes327@gmail.com>
2020-02-04 18:17:44 -0500

    Hacky modification to allow display on own pi
    This is not a proper display setup, this is only a quick hack written 
    specifically for my own pi and inky display. This will be changed before 
    long to support a repository-wide config file to set display 
    configuration.

--- analog.py
+++ analog.py
@@ -3,7 +3,7 @@
 
 """Display an analog clock face on an inky display."""
 
-from PIL import Image, ImageDraw, ImagePalette  # type: ignore
+from PIL import Image, ImageDraw  # type: ignore
 from typing import Tuple
 
 
@@ -17,22 +17,35 @@ def draw_hand(length: int, angle: float, center: Tuple[int, int], image: Image) 
 
     Args:
         length: The length of the hand in pixels
-        angle:  Angle to rotate the hand by
+        angle:  Angle to rotate the hand by in radians
         center: Center point of the clock face
         image:  Image file to draw on to
 
+    Todo:
+        Fancier hand display
+
     """
     draw = ImageDraw.Draw(image)
-    draw.line((center[0], center[1], 104, 52), 1) 
+    draw.line((center[0], center[1], 104, 52), 1)
 
 
 if __name__ == '__main__':
-    palette = 768 * [0]
-    palette[0] = 255
-    palette[1] = 255
-    palette[2] = 255
-    palette[6] = 255
+    palette = 3 * [255]
+    palette += 3 * [0]
+    palette += [166, 152, 1]
+    palette += 759 * [0]
+
     img = Image.new("P", (212, 104), color=0)
     draw_hand(52, 0.0, (52, 52), img)
     img.putpalette(palette)
     img.save('analog.png')
+    try:
+        from inky import InkyPHAT  # type: ignore
+    except RuntimeError:
+        pass
+    except ModuleNotFoundError:
+        pass
+    else:
+        inky_display = InkyPHAT('yellow')
+        inky_display.set_image(img)
+        inky_display.show()
