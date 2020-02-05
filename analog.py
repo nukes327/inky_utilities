090ed761e0157ce6f6fd2109d5c1b50c3f2fd745
nukes327 <jake.nukes327@gmail.com>
2020-02-04 20:38:21 -0500

    Prettier hands
    Hands look much better, next on the list is making use of the extra 
    screen real-estate.
    
    Current thought is some other information often present on fancier 
    analog wristwatches.

--- analog.py
+++ analog.py
@@ -31,7 +31,19 @@ def draw_hand(center: Tuple[int, int], length: int, time: int, image: Image) -> 
     time_offset = time - 15
     outer_x = math.cos(math.radians(6 * time_offset)) * length + center[0]
     outer_y = math.sin(math.radians(6 * time_offset)) * length + center[1]
+
+    diamond_left_x = math.cos(math.radians(6 * time_offset - 5)) * (length - 4) + center[0]
+    diamond_left_y = math.sin(math.radians(6 * time_offset - 5)) * (length - 4) + center[1]
+
+    diamond_right_x = math.cos(math.radians(6 * time_offset + 5)) * (length - 4) + center[0]
+    diamond_right_y = math.sin(math.radians(6 * time_offset + 5)) * (length - 4) + center[1]
+
+    inner_x = math.cos(math.radians(6 * time_offset)) * (length - 8) + center[0]
+    inner_y = math.sin(math.radians(6 * time_offset)) * (length - 8) + center[1]
+
     draw.line([(center[0], center[1]), (outer_x, outer_y)], fill=1)
+    draw.polygon([(outer_x, outer_y), (diamond_right_x, diamond_right_y),
+                  (inner_x, inner_y), (diamond_left_x, diamond_left_y)], fill=2, outline=1)
 
 
 def draw_face(center: Tuple[int, int], radius: int, image: Image) -> None:
@@ -39,6 +51,7 @@ def draw_face(center: Tuple[int, int], radius: int, image: Image) -> None:
 
     Args:
         center: Center point of the clock face
+        radius: Radius of the clock face
         image:  Image to draw on to
 
     """
@@ -61,6 +74,20 @@ def draw_face(center: Tuple[int, int], radius: int, image: Image) -> None:
                    (base_x * radius + center[0], base_y * radius + center[1])], fill=1, width=3)
 
 
+def draw_pin(center: Tuple[int, int], radius: int, image: Image) -> None:
+    """Draw the center pin the hands attach to.
+
+    Args:
+        center: Center point of the pin
+        radius: Radius of the pin
+        image:  Image to draw on to
+
+    """
+    draw = ImageDraw.Draw(image)
+    draw.ellipse([(center[0] - radius, center[1] - radius), (center[0] + radius, center[1] + radius)],
+                 outline=1, fill=2)
+
+
 if __name__ == '__main__':
     palette = 3 * [255]
     palette += 3 * [0]
@@ -69,13 +96,16 @@ if __name__ == '__main__':
 
     img = Image.new("P", (212, 104), color=0)
 
-    now = time.localtime(time.time())
-    hour = (now.tm_hour % 12) * 5
-    minute = now.tm_min
-    draw_hand((52, 52), 48, minute, img)
-    draw_hand((52, 52), 24, hour, img)
-
     draw_face((52, 52), 50, img)
+
+    now = time.localtime(time.time())
+    minute = now.tm_min
+    hour = ((now.tm_hour % 12) + (minute / 60)) * 5
+    draw_hand((52, 52), 48, minute, img)
+    draw_hand((52, 52), 36, hour, img)
+
+    draw_pin((52, 52), 2, img)
+
     img.putpalette(palette)
     img.save('analog.png')
     try:
