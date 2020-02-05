cba7a27dd8dbd9fbee50dc9fad240de9cbc550fd
nukes327 <jake.nukes327@gmail.com>
2020-02-04 19:14:33 -0500

    Quick poly test

--- analog.py
+++ analog.py
@@ -26,7 +26,19 @@ def draw_hand(length: int, angle: float, center: Tuple[int, int], image: Image) 
 
     """
     draw = ImageDraw.Draw(image)
-    draw.line((center[0], center[1], 104, 52), 1)
+    draw.line([(center[0], center[1]), (104, 52)], 1)
+
+
+def draw_face(center: Tuple[int, int], image: Image) -> None:
+    """Draw the face of the clock.
+
+    Args:
+        center: Center point of the clock face
+        image:  Image to draw on to
+
+    """
+    draw = ImageDraw.Draw(image)
+    draw.polygon([(52, 0), (104, 52), (52, 104), (0, 52)], outline=1)
 
 
 if __name__ == '__main__':
@@ -37,6 +49,7 @@ if __name__ == '__main__':
 
     img = Image.new("P", (212, 104), color=0)
     draw_hand(52, 0.0, (52, 52), img)
+    draw_face((52, 52), img)
     img.putpalette(palette)
     img.save('analog.png')
     try:
@@ -48,4 +61,5 @@ if __name__ == '__main__':
     else:
         inky_display = InkyPHAT('yellow')
         inky_display.set_image(img)
+        inky_display.set_border(inky_display.BLACK)
         inky_display.show()
