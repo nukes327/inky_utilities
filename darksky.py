c9a0e2a7988ee52cb7a1751547acb6a290be5454
nukes327 <jake.nukes327@gmail.com>
2020-02-19 19:07:53 -0500

    Darksky API confirmed functional
    Need to pare down what is currently requested from the
    API, bare request gives a 31K JSON which is rather large.
    
    Started adding some logging to code, need to start being
    more thorough with this. Also need to add some options in
    configuration to choose logging level or config file.
    In step with this, need to separate out some shared
    functionality into its own module and put a base
    config-generating function within it.

--- darksky.py
+++ darksky.py
@@ -6,9 +6,16 @@
 import json
 import requests
 import toml
+import logging
+
+logging.config.fileConfig("config/logging.ini")
+logger = logging.getLogger(__name__)
+logger.info("Logging configuration loaded")
+
 
 with open("config/utils.toml", "r") as conffile:
     config = toml.load(conffile)
+logger.info("Configuration loaded")
 
 darksky = config["apis"]["darksky"]
 key = darksky["secret"]
@@ -16,10 +23,12 @@ lat = darksky["latitude"]
 lon = darksky["longitude"]
 
 if key != "Replace me":
+    logger.debug(f"\nKey: {key}\nLatitude: {lat}\nLongitude: {long}")
     res = requests.get(f"https://api.darksky.net/forecast/{key}/{lat},{lon}")
 else:
-    print("While this is open source software a private dev key is required for this util")
+    logger.error("No developer API key present")
     exit(-1)
 
 with open("forecast.json", "w+") as forecast:
+    logger.debug(repr(forecast))
     json.dump(json.loads(res.content), forecast)
